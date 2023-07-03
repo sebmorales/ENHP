9fcc9a8493e4586642189463316e9032cb1d1556
seb <sebmoralesprado@gmail.com>
2023-07-03 08:15:19 -0400

    updated server for 2023 class

--- ENHP_Server/experimentNetPerf.js
+++ ENHP_Server/experimentNetPerf.js
@@ -1,26 +1,9 @@
-//tattoo.live
-
-//Express
-var express = require('express');
-var app = express();
-var https = require('https');
-
-var http = require('http').createServer(app);
-var port = process.env.PORT || 8080;//8099
-// var port = process.env.PORT || 4000;//8099
-var server = app.listen(port);
+let express = require('express');
+let app = express();
+let server = app.listen(process.env.PORT || 8877);
+console.log('server running')
 var fs = require('fs');
-var bodyParser= require("body-parser");
-//ejs
 var ejs = require('ejs');
-
-let window_names=["kale","ginger","onion","cilantro","cabbage","carrot","broccoli","mushroom","potato","tomato","beet","pepper","cauliflower","celery","avocado","spinach","radish","basil","mint","parsley"]
-var windows=[]
-
-//Sockets
-var socket = require('socket.io').listen(server);
-console.log("running on port "+port)
-
 //Setup public lib
 app.use(express.static(__dirname + '/public'));
 //Setup the views folder
@@ -30,68 +13,83 @@ app.set("views", __dirname + '/views');
 app.engine('.html', ejs.__express);
 app.set('view-engine', 'html');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+let boards=[];
+
+var socket = require('socket.io');
+var io = socket(server, {
+    //this allows external websites to connect
+  cors: {
+    origin: '*'
+  },
+  //this allows older socket versions to connect
+  allowEIO3: true
+});
+
+io.sockets.on('connection', newConnection);
+
 
 
 //Router
 app.get("/", function(req, res){
-    console.log("here");
     res.render("windows.html")
 });
-
-app.get("/morakana", function(req, res){
-    res.render("morakana.html")
+app.get("/playback", function(req, res){
+  res.render("playback.html")
 });
 
+function newConnection(socket){
+  io.emit("photo",boards);
 
+  console.log("new Connection")
+  socket.on("message", function(data){
+    //this will send to everyone connected to the site
+    io.emit("message", data)
+    // socket_client.emit("message",data);
+    // socket_nuum.emit("message",data);
+    // console.log(data)
 
-socket.on('connection', function(client){
-    console.log("new connection");
-    socket.emit('allWindos',windows);
-    client.on('getSetup', function(data){
-      socket.emit('allWindos',windows);
+    // if(data.da>0.1){
+    //   // data.bat=data.bat*2.0/4095.0;
+    // console.log(data)
+    // }
+  })
 
-    });
+   socket.on("midi", function(data){
+     // console.log(data);
+     io.emit("midi",data)
+  })
 
+   socket.on("osc", function(data){
+     // console.log(data);
+     io.emit("osc",data)
+  })
 
-
-
-    client.on('photoresistor', function(data){
-        console.log(data);
-        for(let i=0;i<20;i++){
-          if("photo_"+windows[i].name==data.board){
-            windows[i].value=data.photo;
-            let d={};
-            d.name=windows[i].name;
-            d.value=windows[i].value;
-            d.time=Date.now();
-            fs.appendFile('light_history.txt', JSON.stringify(d), function (err) {
-            if (err) throw err;
-              //console.log('Saved!');
-            });
-          }
-        }
-        socket.emit('photoresistor', data);
-    });
-
-
-    client.on('disconnect', function(){
-        console.log("A user dissconected.");
-    });
-});
-
-
-class Window{
-  constructor(name, value){
-    this.name=name;
-    this.value=value;
-  }
- }
- initWindows();
- function initWindows(){
-     for (let i=0;i<window_names.length;i++){
-         windows.push(new Window(window_names[i],-1));
-     }
-     console.log(windows)
- }
+  socket.on("photo",function(data){
+    // console.log(data.name)
+    console.log(data);
+    let board_exists=false;
+    data.time=Date.now();
+    for (let i=0;i<boards.length;i++){
+      if(boards[i].name==data.name){
+        boards[i].photo=data.photo;
+        boards[i].time=data.time;
+        board_exists=true;
+      }
+ 
+    }
+    if(!board_exists){
+      let new_board={
+        "name":data.name,
+        "photo":data.photo,
+        "time":data.time
+      }
+      boards.push(new_board);
+    }
+    fs.appendFile('public/js/photo_history.txt', "\n"+JSON.stringify(data), function (err) {
+    if (err) throw err;
+      // console.log("data saved");
+     });
+     io.emit("photo",boards);
+    //  console.log(data);
+  });
+}
